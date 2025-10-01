de5a0a934aa9575f0f387892e2dc2ffc2734cd27
robk <robk@localhost>
2025-10-01 21:43:22 +0100

    Completed VPC section

--- terraform-aws/06-AWS-VPC/2-vpc-module/c2-generic-variables.tf
+++ terraform-aws/06-AWS-VPC/2-vpc-module/c2-generic-variables.tf
@@ -12,13 +12,13 @@ variable "aws_region" {
 variable "environment" {
   description = "Environment Variable used as a prefix"
   type        = string
-  default     = "dev"
+  default     = "DEV"
 }
 # INFO: Business Division
 variable "business_divsion" {
   description = "Business Division in the large organization this Infrastructure belongs"
   type        = string
-  default     = "SAP"
+  default     = "Operations"
 }
 
 # ! Default values will be overwritten in terraform.tfvars
--- terraform-aws/06-AWS-VPC/2-vpc-module/c3-local-values.tf
+++ terraform-aws/06-AWS-VPC/2-vpc-module/c3-local-values.tf
@@ -1,13 +1,17 @@
 # INFO: Local Values
 # INFO: https://developer.hashicorp.com/terraform/language/block/locals
 
+data "aws_availability_zones" "available" {}
 locals {
   owners      = var.business_divsion
   environment = var.environment
   name        = "${var.business_divsion}-${var.environment}"
-  #name = "${local.owners}-${local.environment}"
+  #name        = "${local.owners}-${local.environment}"
+
+  azs = slice(data.aws_availability_zones.available.names, 0, 2)
+
   common_tags = {
     owners      = local.owners
     environment = local.environment
   }
-} 
+}
