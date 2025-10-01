69e74212f730e652420a948113348cdc1e55b3aa
robk <robk@localhost>
2025-10-01 21:44:49 +0100

    Added INFO about slice function

--- terraform-aws/06-AWS-VPC/2-vpc-module/c3-local-values.tf
+++ terraform-aws/06-AWS-VPC/2-vpc-module/c3-local-values.tf
@@ -1,5 +1,6 @@
 # INFO: Local Values
 # INFO: https://developer.hashicorp.com/terraform/language/block/locals
+# INFO: slice Function used for AZ's: https://developer.hashicorp.com/terraform/language/functions/slice
 
 data "aws_availability_zones" "available" {}
 locals {
