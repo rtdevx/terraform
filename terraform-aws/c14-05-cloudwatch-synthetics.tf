8aaf4f6282e403051c0417d7e1880dea6c567ba6
robk <robk@localhost>
2025-11-05 17:20:47 +0000

    Updated synthetics.

--- terraform-aws/c14-05-cloudwatch-synthetics.tf
+++ terraform-aws/c14-05-cloudwatch-synthetics.tf
@@ -1,7 +1,6 @@
 # INFO: Generate sswebsite code for Canary
 
 /*
-
 ```AWSConsole
 CloudWatch > Application Signals (APM) > Synthetics Canaries > Create Canary
 ```
@@ -9,6 +8,4 @@ CloudWatch > Application Signals (APM) > Synthetics Canaries > Create Canary
 1. Create a canary and copy-paste the generated script to:
 
 `terraform-aws\sswebsite2\nodejs\node_modules\sswebsite2.js`
-
-
 */
