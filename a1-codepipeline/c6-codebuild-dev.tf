08751afa2ff9e0d6fb58e4e47e4445ce362369ea
RobK <johndoe@example.com>
2025-11-19 16:21:21 +0000

    Updated CodePipeline related stuff.

---
+++ a1-codepipeline/c6-codebuild-dev.tf
@@ -0,0 +1,22 @@
+resource "aws_codebuild_project" "dev" {
+  name         = "MyCodeBuildProject-dev"
+  description  = "Build project for Node.js app"
+  service_role = aws_iam_role.codebuild_role.arn
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "aws/codebuild/standard:5.0"
+    type         = "LINUX_CONTAINER"
+  }
+
+  source {
+    type      = "GITHUB"
+    location  = "https://github.com/rtdevx/terraform-iacdevops-with-aws-codepipeline/tree/main"
+    buildspec = "buildspec-dev.yml"
+  }
+
+  artifacts {
+    type     = "S3"
+    location = aws_s3_bucket.codepipeline.bucket
+  }
+}
